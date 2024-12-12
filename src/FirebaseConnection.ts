54a4cbfbf3e28136d44376e22567d347a1a8d916
Filipe Grolli <46730932+filipegrolli@users.noreply.github.com>
2024-12-12 15:06:40 -0300

    Update FirebaseConnection.ts
    Remove tokens

--- src/FirebaseConnection.ts
+++ src/FirebaseConnection.ts
@@ -4,13 +4,13 @@ import { getAuth, Auth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC9l38jqd7HdcEb7YCdO7YFqZiuRgUGmJM",
-  authDomain: "todolist-14b6a.firebaseapp.com",
-  projectId: "todolist-14b6a",
-  storageBucket: "todolist-14b6a.firebasestorage.app",
-  messagingSenderId: "611825714284",
-  appId: "1:611825714284:web:c7e261ea447a72a100c449",
-  measurementId: "G-NEBYE7VT52"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app: FirebaseApp = initializeApp(firebaseConfig);
